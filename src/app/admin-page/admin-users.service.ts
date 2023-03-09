81f681730c269486fddea72e8548b32718aab062
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-03-09 17:03:24 +0400

    based get request

--- src/app/admin-page/admin-users.service.ts
+++ src/app/admin-page/admin-users.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class AdminUsersService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  postDataProfile(token: string){
+    const tokenA = 'Bearer ' + token;
+    console.log(tokenA);
+    return this._http.get('http://learn-golang.eu-central-1.elasticbeanstalk.com/api/admin/users', { headers: new HttpHeaders({ 'Authorization': tokenA })} )
+  }
 }
