930a42261d9072d9f11689e930723362f65bb7be
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-03-09 17:04:32 +0400

    subscriber on token

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserResolveService } from 'src/app/sign-in/user-resolve.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   public isShown = false;
   public userIconNav = false;
+
+  constructor (private _token: ActivatedRoute) {
+    this._token.data.subscribe(data => console.log(data)); // An observable of the static and resolved data of this route.
+    //сделать нормальную передачу данных
+  }
+  
+  ngOnInit() {
+    console.log(this._token.data.subscribe(data => console.log(data)));
+  }
 }
