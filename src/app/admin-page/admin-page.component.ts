3784c494ae6fcf78ecd9ab0e8d44c33e83c3fb49
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-03-14 16:31:57 +0400

    add resolve users

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserResolveService } from '../sign-in/user-resolve.service';
+import { AdminUsersService } from './admin-users.service';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.css']
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
+
+  
+  public statusToken: any;
+  public tokenDataProfile: any;
+
+  public user: any = {
+    name: 'Bob'
+  };
+  public users: any = [
+    {name: 'Bob'},
+    {name: 'Jopa'},
+    {name: 'Pica'},
+  ];
+
+  constructor(private _users: AdminUsersService, private readonly _resolveService: UserResolveService) {}
+
+  someF() {
+    console.log('work!');
+    this._users.getUsers().subscribe( tokenDataProfile => this.users = tokenDataProfile);
+  }
+
+  someTg() {
+    this._users.ngOnInit();
+  }
+
+  ngOnInit() {
+    this._resolveService.userProfile$.subscribe(
+      value => {
+        if (value.data.user !== undefined) {
+          if (value.data.user.role === 'admin') {
+          }
+          console.log(value);
+        }
+      }
+    );
+    console.log(this.tokenDataProfile);
+  }
 
 }
--- src/app/admin-page/admin-users.service.ts
+++ src/app/admin-page/admin-users.service.ts
@@ -1,16 +1,61 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { UserResolveService } from '../sign-in/user-resolve.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminUsersService {
+export class AdminUsersService implements OnInit{
 
-  constructor(private _http: HttpClient) { }
+  public statusToken: any;
+  public tokenDataProfile: any;
 
-  postDataProfile(token: string){
-    const tokenA = 'Bearer ' + token;
-    console.log(tokenA);
-    return this._http.get('http://learn-golang.eu-central-1.elasticbeanstalk.com/api/admin/users', { headers: new HttpHeaders({ 'Authorization': tokenA })} )
+  private users = [
+    { name: 'Bob'},
+    { name: 'Jonh'}
+  ]
+
+  constructor(private _http: HttpClient, private readonly resolveService: UserResolveService) { }
+
+  ngOnInit() { // Залупа не хочет ловить данные
+    this.resolveService.userProfile$.subscribe(
+      value => {
+        if (value.data.user !== undefined) {
+          if (value.data.user.role === 'admin') {
+            this.tokenDataProfile = value;
+            this.resolveService.token$.subscribe(token => this.statusToken = token);
+          }
+        }
+      }
+    );
+    console.log(this.statusToken);
+  }
+
+  getUsers(){
+    /*
+    let promiseAdminToken = new Promise((resolve) => {
+      this.resolveService.userProfile$.subscribe(
+        value => {
+          console.log(value);
+          
+    console.log('work!');
+          if (value.data.user !== undefined) {
+            if (value.data.user.role === 'admin') {
+              
+    console.log('work!');
+              this.resolveService.token$.subscribe(token => resolve(this.statusToken = token));
+            }
+          }
+        }
+      );
+    });
+    
+    console.log('work!');
+    console.log(await promiseAdminToken);
+*/
+    const tokenUser = 'Bearer ' + this.statusToken?.token;
+
+    return this._http.get('http://learn-golang.eu-central-1.elasticbeanstalk.com/api/admin/users', { headers: new HttpHeaders({ 'Authorization': tokenUser })});
+
   }
 }
