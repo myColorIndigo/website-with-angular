29def0ca04e631df172f358e54faac2f00a72c77
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-03-14 16:30:53 +0400

    add user card form

--- src/app/admin-page/user-card/user-card.component.ts
+++ src/app/admin-page/user-card/user-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css']
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
 
+  @Input() user: any;
+
+  constructor() {
+    console.log('constructor: ', this.user);
+  }
+
+  ngOnInit(){
+    console.log('ngOnInit: ', this.user);
+  }
 }
