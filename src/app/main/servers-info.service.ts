fa9107eef32bee8f22be6247cb8d8ba9d19f6d36
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-03 17:31:16 +0400

    add main servise

---
+++ src/app/main/servers-info.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import 'src/app/arrayServers.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServersInfoService {
+
+  constructor() { }
+
+  getServers() {
+
+  }
+}
