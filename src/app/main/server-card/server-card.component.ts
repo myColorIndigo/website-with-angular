fbe98fb4167fb48be354f9d7abfaa794b9dac8dc
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-08 22:54:12 +0400

    add data server

--- src/app/main/server-card/server-card.component.ts
+++ src/app/main/server-card/server-card.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ServerCardComponent {
 
+  public server: any = {
+        "name": "CS2 RETAKE #2",
+        "status": 1,
+        "address": "23.88.25.137:27026",
+        "game": "CS2",
+        "online": 0,
+        "map": "dust_2",
+        "location": "Germany",
+        "hosting": "not found",
+        "id": 296553,
+        "update": 1680461173,
+        "ping": 30,
+        "upTime": 100,
+        "rang": 1,
+        "globalRang": 3,
+        "site": "none"
+    };
+
 }
