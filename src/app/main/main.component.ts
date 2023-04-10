6dda75aecf6cef8307a3952eb2cc4ed1793064cd
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-10 16:24:40 +0400

    relocate array servers and change ways

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,74 +13,22 @@ export class MainComponent implements AfterViewInit , OnInit, OnDestroy {
   
   public searchInputData: string = '';
   public toggleList: boolean = true; // Возможно стоит к кнопкам фильров применить *ngIf и сделать toggle
-  public servers: any = [
-    {
-        "name": "CS2 RETAKE #2",
-        "status": 1,
-        "address": "23.88.25.137:27026",
-        "game": "CS2",
-        "online": 0,
-        "map": "dust_2",
-        "location": "Germany",
-        "hosting": "not found",
-        "id": 296553,
-        "update": 1680461173,
-        "ping": 30,
-        "upTime": 100,
-        "rang": 1,
-        "globalRang": 3,
-        "site": "none"
-    },
-    {
-        "name": "CS2 RETAKE #3",
-        "status": 0,
-        "address": "23.88.25.137:35235",
-        "game": "CS2",
-        "online": 2,
-        "map": "dust_2",
-        "location": "Germany",
-        "hosting": "not found",
-        "id": 154553,
-        "update": 1680461173,
-        "ping": 65,
-        "upTime": 100,
-        "rang": 1,
-        "globalRang": 3,
-        "site": "none"
-    },
-    {
-        "name": "CS2 DM #1",
-        "status": 1,
-        "address": "23.88.25.137:68326",
-        "game": "CS2",
-        "online": 6,
-        "map": "dust_2",
-        "location": "GB",
-        "hosting": "not found",
-        "id": 296343,
-        "update": 1680461173,
-        "ping": 4,
-        "upTime": 100,
-        "rang": 23,
-        "globalRang": 42,
-        "site": "none"
-    }
-
-];
+  public servers: any = [];
+  
   buttonSubscription: any;
 
   constructor(private elm: ElementRef, private _servers: ServersInfoService) {}
 
   search() {
-    let serversCount: Array<object> = [];
-    let searchForName = this.servers.map((item: { name: string; }) => item.name).filter((word: string) => {
+    let serversCount: any = [];
+    let searchForName = this._servers.servers.map((item: { name: string; }) => item.name).filter((word: string) => {
       if (word.toLowerCase().indexOf(this.searchInputData.toLowerCase()) !== -1) {
         console.log(word);
-        for (let i = 0; i < this.servers.length; i++) {
+        for (let i = 0; i < this._servers.servers.length; i++) {
 
-          if (this.servers[i].name === word) {
-            console.log(this.servers[i]);
-            serversCount.push(this.servers[i]);
+          if (this._servers.servers[i].name === word) {
+            console.log(this._servers.servers[i]);
+            serversCount.push(this._servers.servers[i]);
             console.log(serversCount);
           }
           
@@ -93,6 +41,7 @@ export class MainComponent implements AfterViewInit , OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.servers = this._servers.getServers();
     const myInput = document.getElementById('SearchServer') as HTMLInputElement;
     const search = fromEvent(myInput, 'input');
     search.pipe(debounceTime(400)).subscribe((e: Event) => {
