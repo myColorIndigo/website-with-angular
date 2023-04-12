81be713961030b63a81f5c55c37ea21964173ef6
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-12 22:30:35 +0400

    add activated route

--- src/app/main/server-card/server-card.component.ts
+++ src/app/main/server-card/server-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-server-card',
@@ -25,4 +26,7 @@ export class ServerCardComponent {
         "site": "none"
     };
 
+    constructor(private route: ActivatedRoute) {
+      this.route.params.subscribe(params => console.log(params));
+    }
 }
