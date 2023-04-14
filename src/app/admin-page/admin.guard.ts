e9587e4f580e416a7a162d6e6e1af7e02fb5cead
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-14 22:23:10 +0400

    add import admin service

--- src/app/admin-page/admin.guard.ts
+++ src/app/admin-page/admin.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AdminUsersService } from './admin-users.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+  constructor(private adminService: AdminUsersService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.adminService.adminGuard();
   }
   
 }
