b9b99c2835e3b013d161e65e8f11b8be084f883c
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-15 20:16:51 +0400

    change service

--- src/app/admin-page/admin.guard.ts
+++ src/app/admin-page/admin.guard.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AdminUsersService } from './admin-users.service';
+import { UserDataInService } from '../sign-in/user-data-in.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private adminService: AdminUsersService) {}
+  constructor(private guard: UserDataInService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.adminService.adminGuard();
+    return this.guard.guardAdmin();
   }
   
 }
