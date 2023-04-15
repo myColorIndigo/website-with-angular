d3b331882d6bdc33a400bcec8a89e6fd57c4c06e
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-15 20:34:34 +0400

    add service function

--- src/app/sign-in/user.guard.ts
+++ src/app/sign-in/user.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserDataInService } from './user-data-in.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+
+  constructor(private guard: UserDataInService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+    return this.guard.guardUser();
+  } // Не совсем правильно работает, надо будет изменить
   
 }
