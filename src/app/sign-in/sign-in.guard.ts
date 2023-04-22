319c623bbcf26947b995dbbfa29efe55f13265ae
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-22 18:00:59 +0400

    added constructor in guard

--- src/app/sign-in/sign-in.guard.ts
+++ src/app/sign-in/sign-in.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserDataInService } from './user-data-in.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignInGuard implements CanActivate {
+
+  constructor(private guard: UserDataInService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.guard.guardSignIn();
   }
   
 }
