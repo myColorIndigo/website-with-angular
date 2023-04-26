c2e1404cf2fbc62236c6ec32a9187df41c51bfd3
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-26 22:43:07 +0400

    added event for input

--- src/app/main/server-card/add-server-card/add-server-card.component.ts
+++ src/app/main/server-card/add-server-card/add-server-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-add-server-card',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-server-card.component.css']
 })
 export class AddServerCardComponent {
-  // Сделать оформление для редактора добавления шаблона
+  
+  constructor() {}
+
+  addServer() {
+    console.log('on');
+    const myServer = document.getElementById('serverID') as HTMLInputElement;
+    fromEvent(myServer, 'input').subscribe((e: Event) => {
+      let inputContent = (e.target as HTMLInputElement).value;
+      console.log(inputContent);
+    });
+  }
 }
