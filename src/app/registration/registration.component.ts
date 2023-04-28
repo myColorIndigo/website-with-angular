cb2d59cebcd28addb63a553818f2291436a3e12a
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-28 20:58:53 +0400

    added function validator

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,12 +8,36 @@ import { NgForm } from '@angular/forms';
 })
 export class RegistrationComponent {
 
+  public nameInvalid: boolean = false;
+  public emailInvalid: boolean = false;
+  public repeatPasswordInvalid: boolean = false;
+
   constructor() {}
   
   addUser(form: NgForm) {
-    console.log(form);
-    console.log(form.controls['name'].valid);
-    console.log(form.controls['name'].value);
+    //console.log(form);
     //console.log(form.controls['email'].valid);
+
+    if (form.controls['name'].valid) {
+      this.nameInvalid = false;
+    } else {
+      this.nameInvalid = true;
+    }
+
+    if (form.controls['email'].valid) {
+      this.emailInvalid = false;
+    } else {
+      this.emailInvalid = true;
+    }
+
+    if (form.controls['password'].value === form.controls['repeatPassword'].value) {
+      this.repeatPasswordInvalid = false;
+    } else {
+      this.repeatPasswordInvalid = true;
+    }
+
+    if (form.valid === true) {
+      return console.log('good!');
+    }
   }
 }
