c21c10ad408f5863b04dbea00927dd3bf095b659
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-28 21:14:43 +0400

    added registration service in providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
 
 import { ConfigComponent } from './config/config.component';
-import { VersionService } from './config/version.service';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './header/nav/nav.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { UserDataInService } from './sign-in/user-data-in.service';
 import { RegistrationComponent } from './registration/registration.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
-import { UserResolveService } from './sign-in/user-resolve.service';
 import { UserCardComponent } from './admin-page/user-card/user-card.component';
 import { InWorkComponent } from './in-work/in-work.component';
 import { SettingsComponent } from './settings/settings.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ServersInfoService } from './main/servers-info.service';
 import { ServerCardComponent } from './main/server-card/server-card.component';
+import { AddServerCardComponent } from './main/server-card/add-server-card/add-server-card.component';
+
+import { VersionService } from './config/version.service';
+import { UserDataInService } from './sign-in/user-data-in.service';
+import { UserResolveService } from './sign-in/user-resolve.service';
+import { ServersInfoService } from './main/servers-info.service';
+import { AddServerService } from './main/server-card/add-server-card/add-server.service';
+import { RegistrationService } from './registration/registration.service';
+
 import { AdminGuard } from './admin-page/admin.guard';
 import { UserGuard } from './sign-in/user.guard';
-import { AddServerCardComponent } from './main/server-card/add-server-card/add-server-card.component';
-import { AddServerService } from './main/server-card/add-server-card/add-server.service';
 
 @NgModule({
   declarations: [
@@ -55,7 +58,7 @@ import { AddServerService } from './main/server-card/add-server-card/add-server.
     FormsModule,
     AppRoutingModule
   ],
-  providers: [VersionService, UserDataInService, UserResolveService, ServersInfoService, AddServerService,  UserGuard, AdminGuard],
+  providers: [VersionService, UserDataInService, UserResolveService, ServersInfoService, AddServerService, RegistrationService,  UserGuard, AdminGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
