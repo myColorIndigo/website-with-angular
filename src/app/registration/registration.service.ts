a5ce7e62b3769383490604e36f786bb94df33336
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-28 21:31:00 +0400

    added function for post user data

--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+export class User {
+  name!: string
+  email!: string
+  password!: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
   constructor() { }
+
+  postData(user: User) {
+
+    const body = {name: user.name, email: user.email, password: user.password};
+
+    return console.log(body);
+  }
 }
