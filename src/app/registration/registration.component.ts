2b17b975b71dbfb80f4c633df5ad56e2dabd3904
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-04-28 17:29:05 +0400

    based function for registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export class User {
+  name!: string;
+  email!: string;
+  password!: string;
+  repeatPassword!: string;
+}
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -7,4 +14,14 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+  user: User = new User();
+
+  constructor() {}
+  
+  addUser() {
+    if(this.user.password === this.user.repeatPassword) {
+      console.log(this.user);
+
+    }
+  }
 }
