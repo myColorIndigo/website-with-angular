3d5ef9aefe39677ccb2a42b65640807aba5745a4
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-04 21:23:15 +0400

    added fake user avatar in profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
   // Сделать запрос на установку аватарки из хранилища и собственно прописать замену аватара (возможно через обсервабл с получением обновленных данных аватара из хранилища)
+
+  public imgAvatar: any = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    // Простая форма установки аватара (фейкового):
+    this.imgAvatar = sessionStorage.getItem('userAvatar');
+  }
+
 }
