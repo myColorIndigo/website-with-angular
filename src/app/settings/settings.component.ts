87087c9ce983bb3f90d95fe16e338e25d7c6d5f2
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-04 21:44:37 +0400

    added fake avatar in settings from storage

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   // Сделать функционал замены аватарки, отправку данных на сервер и возможно моментальную замену в хранилище
+
+  public imgAvatar: any = '';
+
+  constructor() {}
+
+  ngOnInit() { // Получение фейкового аватара из хранилища:
+    this.imgAvatar = sessionStorage.getItem('userAvatar');
+  }
+
 }
