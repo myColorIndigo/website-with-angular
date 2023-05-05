a74fd7b141f3c61ba59b56582aaf270f51dc4a14
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-05 22:15:54 +0400

    added dropbox form

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -9,10 +9,8 @@ import { SettingsService } from './settings.service';
 })
 export class SettingsComponent implements OnInit {
   // Сделать функционал замены аватарки, отправку данных на сервер и возможно моментальную замену в хранилище
-  // Подпункты: Разобраться с функционалом внесения файла через HTML
-  //            Сделать сервис с передачей обновленных данных (файла) на сервер
-  //            Фейковое обновление данных на сервере и внесение изменений в компоненте с заменой строки аватара 
 
+  public imgAvatarUrl: any = '';
   public imgAvatar: any = '';
 
   constructor(private _data: SettingsService) {}
@@ -28,13 +26,62 @@ export class SettingsComponent implements OnInit {
       console.log(profileForm);
     }
 
-    if (profileForm.valid) {
-      this._data.postData(profileForm.value);
+    if (profileForm.valid === true) { // Переделать условия, тк валидация всегда true
+      this._data.postData(profileForm.value).subscribe(value => {
+        console.log(value);
+        // sessionStorage.setItem('userAvatar', value); 
+      }); // Возможно переделать получение данных для замены ссылки на актуальный аватар
     }
   }
 
   ngOnInit() { // Получение фейкового аватара из хранилища:
-    this.imgAvatar = sessionStorage.getItem('userAvatar');
+    this.imgAvatarUrl = sessionStorage.getItem('userAvatar');
+
+    this.dropboxAvatar();
   }
 
+  // Функция получения файла:
+  uploadFileAvatar() {
+    const selectedFile = document.getElementById('file-upload') as HTMLInputElement;
+    if (selectedFile.files?.length !== 0 && selectedFile.files !== null) {
+      console.log(selectedFile.files[0]);
+      return selectedFile.files[0];
+    }
+    return false;
+  }
+
+  // Функции под дропбокс:
+
+  dropboxAvatar() {
+    const dropbox = document.getElementById("dropbox") as HTMLElement;
+    if (dropbox !== null) {
+      dropbox.addEventListener("dragenter", this.dragenter, false);
+      dropbox.addEventListener("dragover", this.dragover, false);
+      dropbox.addEventListener("drop", this.drop, false);
+    }
+  }
+
+  dragenter(e: any) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  
+  dragover(e: any) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  drop(e: any) {
+    e.stopPropagation();
+    e.preventDefault();
+  
+    let dt = e.dataTransfer;
+    let files = dt.files;
+  
+    this.imgAvatar = files[0];
+    console.log(this.imgAvatar);
+
+    // console.log(files); Не хватает только анимации дропа файла и ограничения в 1 файл
+    // handleFiles(files);
+  }
 }
