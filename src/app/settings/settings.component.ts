2a4c460682faf67e8661455b4e4751fcafce9586
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-05 16:46:26 +0400

    added form for settings avatar

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -7,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
   // Сделать функционал замены аватарки, отправку данных на сервер и возможно моментальную замену в хранилище
+  // Подпункты: Разобраться с функционалом внесения файла через HTML
+  //            Сделать сервис с передачей обновленных данных (файла) на сервер
+  //            Фейковое обновление данных на сервере и внесение изменений в компоненте с заменой строки аватара 
 
   public imgAvatar: any = '';
 
   constructor() {}
+
+  onSubmit(profileForm: NgForm) {
+    const selectedFile = document.getElementById('file-upload') as HTMLInputElement;
+    
+    console.log(profileForm);
+    
+    if (selectedFile.files?.length !== 0 && selectedFile.files !== null) {
+      console.log(selectedFile.files[0]);
+      
+    }
+  }
 
   ngOnInit() { // Получение фейкового аватара из хранилища:
     this.imgAvatar = sessionStorage.getItem('userAvatar');
