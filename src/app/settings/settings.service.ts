05de340f003a4daf23859369a3896643b612c6d5
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-06 20:56:16 +0400

    add post for personal info and notifications data

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -8,7 +8,19 @@ export class SettingsService {
 
   constructor(private _http: HttpClient) { }
 
-  postData(valueData: any) {
+  // Отправка формы профиля:
+  postDataProfile(valueData: any) {
     return this._http.post<any>('https://jsonplaceholder.typicode.com/posts', valueData);
   }
+
+  // Отправка формы персональной информации:
+  postDataPersonalInfo(valueData: any) {
+    return this._http.post<any>('https://jsonplaceholder.typicode.com/posts', valueData);
+  }
+
+  // Отправка формы уведомлений:
+  postDataNotifications(valueData: any) {
+    return this._http.post<any>('https://jsonplaceholder.typicode.com/posts', valueData);
+  }
+
 }
