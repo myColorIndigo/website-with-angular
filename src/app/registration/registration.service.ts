f3b2e4cbd8999eeddeecdc60d2626bf3759ea711
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-09 20:53:06 +0400

    added set value in storage, router and resolve

--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserResolveService } from '../sign-in/user-resolve.service';
 
 export class User {
   name!: string
@@ -12,12 +14,35 @@ export class User {
 })
 export class RegistrationService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient, private router: Router, private readonly userResolveService: UserResolveService) { }
 
   postData(user: User) {
 
     const body = {name: user.name, email: user.email, password: user.password};
 
-    return this._http.post('https://hasu.monster/api/register', body); // Заменить на передачу данных на сервер и добавить гарды 
+    return this._http.post('https://hasu.monster/api/register', body).subscribe(value => {
+      console.log(value);
+      this.userStorage(body, value);
+    }); // Заменить на передачу данных на сервер и добавить гарды 
+  }
+  // Запись данных в хранилище:
+  userStorage(userData: any, serverData: any) {
+    sessionStorage.setItem('userName', userData.name);
+    sessionStorage.setItem('userRole', '0');
+    sessionStorage.setItem('userEmail', userData.email);
+    sessionStorage.setItem('userToken', serverData.token);
+
+    console.log(sessionStorage);
+    
+    this.guardUser();
+
+    this.router.navigate(['']);
+
+    return this.userResolveService.takeDataProfile(userData);
+  }
+
+  // Гарды юзера при регистрации:
+  guardUser() {
+    
   }
 }
