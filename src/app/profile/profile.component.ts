c239882cc2b7db6325b2c62fdedbd704e8c84d84
myColorIndigo <67101532+myColorIndigo@users.noreply.github.com>
2023-05-09 17:09:03 +0400

    added name and email user form from storage

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,12 +9,16 @@ export class ProfileComponent implements OnInit{
   // Сделать запрос на установку аватарки из хранилища и собственно прописать замену аватара (возможно через обсервабл с получением обновленных данных аватара из хранилища)
 
   public imgAvatar: any = '';
+  public userName: any = '';
+  public userEmail: any = '';
 
   constructor() {}
 
   ngOnInit() {
     // Простая форма установки аватара (фейкового):
     this.imgAvatar = sessionStorage.getItem('userAvatar');
+    this.userName = sessionStorage.getItem('userName');
+    this.userEmail = sessionStorage.getItem('userEmail');
   }
 
 }
